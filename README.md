bbe13ba09f3bc547123785d142b6354e2c8d3c71
William Denton <wtd@pobox.com>
2013-09-08 12:13:22 -0400

    Direct conversion to GeoJSON; some docs

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 Greenhouse gas emissions data.
 
+## 2011 Canada Greenhouse Gas Emissions
+
+Source: http://maps-cartes.ec.gc.ca/indicators-indicateurs/TableView.aspx?ID=1
+
+Raw: [Greenhouse\_Gas\_Emissions.tsv] (it downloads incorrectly as .xls)
+
+GeoJSON: [2011_canada_ghg_emissions.geojson]
+
+Conversion:
+
+1. Download raw data from [http://maps-cartes.ec.gc.ca//CESI_Services/DataService/1/en]
+1. `ec-ghg-to-geojson.rb > 2011_canada_ghg_emissions.geojson`
+1. Test JSON with `jsonlint 2011_canada_ghg_emissions.geojson`
+
+(Note to self: `curl` doesn't download the whole file for me; stops with exit code 18 ("Partial file") ... why?)
+
--- ec-ghg-to-geojson.rb
+++ ec-ghg-to-geojson.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'csv'
+require 'json'
 
 ghg_data = File.open("Greenhouse_Gas_Emissions.tsv", "r:ISO-8859-1").read
 
@@ -38,12 +39,46 @@ csv = CSV.parse(ghg_data, {:col_sep => "\t", :headers => true, :header_converter
 #   latitude
 #   longitude
 
-
 # Dump fresh, clean CSV to STDOUT, with new headers
-puts csv.headers.to_csv
+# puts csv.headers.to_csv
+# csv.each do |row|
+#   puts row.to_csv
+# end
+
+features = []
+
 csv.each do |row|
-  puts row.to_csv
+
+  properties = {
+    "facility_id" => row[:facility_id],
+    "organisation_name" => row[:organisation_name],
+    "facility_name" => row[:facility_name],
+    "province" => row[:province],
+    "city" => row[:city],
+    "address" => row[:address],
+    "postal_code" => row[:postal_code],
+    "npri_id" => row[:npri_id],
+    "2011_ghg_emissions_kilotonnes_of_carbon_dioxide_equivalent" => row[:'2011_ghg_emissions_kilotonnes_of_carbon_dioxide_equivalent'].to_f,
+    "facility_ghg_data_link" => row[:facility_ghg_data_link],
+    "naics_name" => row[:naics_name],
+    "naics_data_link" => row[:naics_data_link],
+  }
+  geometry = {
+    "type" => "Point",
+    "coordinates" => [row[:longitude].to_f, row[:latitude].to_f],
+  }
+  feature = {
+    "type" => "Feature",
+    "properties" => properties,
+    "geometry" => geometry
+  }
+  features << feature
 end
 
-exit
+output = { 
+  "type" => "FeatureCollection",
+  "features" => features
+}
+
+puts output.to_json
 
